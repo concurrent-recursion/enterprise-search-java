523a49a3974465539df55eede068d509016f442c
Brad Hall <bhall@sae.org>
2022-11-15 09:21:27 -0500

    Fixed date serialization issue for DateValueFilter

--- src/main/java/co/elasticsearch/enterprisesearch/client/model/request/filter/DateValueFilter.java
+++ src/main/java/co/elasticsearch/enterprisesearch/client/model/request/filter/DateValueFilter.java
@@ -8,15 +8,23 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @Accessors(chain = true)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class DateValueFilter implements Filter {
-    @JsonValue
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX", with = {JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY,JsonFormat.Feature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED})
     private List<OffsetDateTime> values = new ArrayList<>();
+
+    @JsonValue
+    public List<String> getValuesJson(){
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+        return values.stream().map(format::format).collect(Collectors.toList());
+    }
 }
